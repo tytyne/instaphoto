da82cc022da3218c1bcf7bb99d42fb9cb8692174
tytyne <dusaflora2@gmail.com>
2019-03-29 11:46:36 +0200

    add profile class

--- photos/models.py
+++ photos/models.py
@@ -48,5 +48,28 @@ class PhotosLetterRecipients(models.Model):
     email = models.EmailField()        
    
   
-   
+class Profile(models.Model):
+    profile_pic = models.ImageField(upload_to = 'profile_pic/', null = True)
+    bio = models.TextField(max_length = 500, blank = True, null = True)
+    user = models.OneToOneField(User, on_delete = models.CASCADE, null = True)
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    @classmethod
+    def get_profiles(cls):
+        profiles = cls.objects.all()
+        return profiles
+
+    @classmethod
+    def search_profiles(cls, query):
+        profile = cls.objects.filter(user__username__icontains=query)
+        return profile
+
+    def __str__(self):
+        return self.user.username
+ 
      

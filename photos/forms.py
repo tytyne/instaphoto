a18b992389f377e37501ab955c7b66f0d586f7e0
tytyne <dusaflora2@gmail.com>
2019-03-29 12:04:43 +0200

    define the name profile

--- photos/forms.py
+++ photos/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Image,Profile
+from .models import Image,Profile.
 
 class PhotosLetterForm(forms.Form):
     your_name = forms.CharField(label='First Name',max_length=30)

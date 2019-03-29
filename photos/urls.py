a82c00a8637521a50f123391489948fa6731b605
tytyne <dusaflora2@gmail.com>
2019-03-29 12:11:28 +0200

    add upload url

--- photos/urls.py
+++ photos/urls.py
@@ -10,6 +10,7 @@ urlpatterns=[
     url(r'^search/', views.search_results, name='search_results'),   
     url(r'^image/(\d+)',views.image,name ='image'),
     url(r'^photo/image$', views.photo_image, name='photo_image'),   
+    url(r'^upload/profile', views.upload_profile, name='upload_profile'),
  
 ]
 if settings.DEBUG:
